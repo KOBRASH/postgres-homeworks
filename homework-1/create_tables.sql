1f6915763c486fed815e9d5289d414e21cf89f96
Sergei Aslanov <courageous@bk.ru>
2023-10-22 21:38:51 +0200

    .

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,30 @@
 -- SQL-команды для создания таблиц
+-- Создание таблицы employees с указанием первичного ключа employee_id
+CREATE TABLE employees (
+    employee_id SERIAL PRIMARY KEY, -- Первичный ключ
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    department_id INT
+);
+
+-- Создание таблицы customers с указанием первичного ключа customer_id
+CREATE TABLE customers (
+    customer_id SERIAL PRIMARY KEY, -- Первичный ключ
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    email VARCHAR(100)
+);
+
+-- Создание таблицы orders с указанием первичного ключа order_id
+CREATE TABLE orders (
+    order_id SERIAL PRIMARY KEY, -- Первичный ключ
+    order_date DATE,
+    customer_id INT,
+    employee_id INT,
+
+    -- Добавление внешнего ключа customer_id, который ссылается на таблицу customers
+    FOREIGN KEY (customer_id) REFERENCES customers(customer_id),
+
+    -- Добавление внешнего ключа employee_id, который ссылается на таблицу employees
+    FOREIGN KEY (employee_id) REFERENCES employees(employee_id)
+);
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,31 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import psycopg2
+
+# Параметры подключения к базе данных PostgreSQL
+db_params = {
+    "host": "localhost",
+    "database": "north",
+    "user": "postgres",
+    "password": "12345"
+}
+
+# Подключение к базе данных
+conn = psycopg2.connect(**db_params)
+cursor = conn.cursor()
+
+# Вставка данных в таблицу employees
+cursor.execute("INSERT INTO employees (first_name, last_name, department_id) VALUES ('John', 'Doe', 1)")
+cursor.execute("INSERT INTO employees (first_name, last_name, department_id) VALUES ('Jane', 'Smith', 2)")
+
+# Вставка данных в таблицу customers
+cursor.execute("INSERT INTO customers (first_name, last_name, email) VALUES ('Alice', 'Johnson', 'alice@example.com')")
+cursor.execute("INSERT INTO customers (first_name, last_name, email) VALUES ('Bob', 'Smith', 'bob@example.com')")
+
+# Вставка данных в таблицу orders
+cursor.execute("INSERT INTO orders (order_date, customer_id, employee_id) VALUES ('2023-01-15', 1, 1)")
+cursor.execute("INSERT INTO orders (order_date, customer_id, employee_id) VALUES ('2023-02-20', 2, 2)")
+
+# Завершение транзакции и закрытие соединения
+conn.commit()
+cursor.close()
+conn.close()
