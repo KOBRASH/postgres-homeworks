bd83713e48dcea440ca8a4c6b4e24f84f7676aaa
KOBRASH <143819964+KOBRASH@users.noreply.github.com>
2023-11-22 15:05:05 +0100

    Update alter_northwind.sql

--- homework-4/alter_northwind.sql
+++ homework-4/alter_northwind.sql
@@ -1,12 +1,22 @@
--- Подключиться к БД Northwind и сделать следующие изменения:
 -- 1. Добавить ограничение на поле unit_price таблицы products (цена должна быть больше 0)
-
+ALTER TABLE products
+ADD CONSTRAINT check_unit_price_positive CHECK (unit_price > 0);
 
 -- 2. Добавить ограничение, что поле discontinued таблицы products может содержать только значения 0 или 1
-
+ALTER TABLE products
+ADD CONSTRAINT check_discontinued_values CHECK (discontinued IN (0, 1));
 
 -- 3. Создать новую таблицу, содержащую все продукты, снятые с продажи (discontinued = 1)
-
+CREATE TABLE discontinued_products AS
+SELECT *
+FROM products
+WHERE discontinued = 1;
 
 -- 4. Удалить из products товары, снятые с продажи (discontinued = 1)
--- Для 4-го пункта может потребоваться удаление ограничения, связанного с foreign_key. Подумайте, как это можно решить, чтобы связь с таблицей order_details все же осталась.
+DELETE FROM products
+WHERE discontinued = 1;
+-- Перенос ограничения foreign_key на временную таблицу
+-- Затем удаление товаров с таблицы products
+-- Например, если есть ограничение foreign key с именем fk_order_details_product_id,
+-- то сначала выполняем: ALTER TABLE order_details DROP CONSTRAINT fk_order_details_product_id;
+-- После удаления товаров возвращаем ограничение: ALTER TABLE order_details ADD CONSTRAINT fk_order_details_product_id FOREIGN KEY (product_id) REFERENCES products (product_id);
